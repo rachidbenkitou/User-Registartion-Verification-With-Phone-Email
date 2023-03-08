21b523f0ce4d1c6f903617d617bff113842d3b85
Rachid Benkitou <rachidbenkitou1@gmail.com>
2023-03-08 22:03:07 +0100

    first commit

---
+++ src/main/java/com/bokito/verification/PhoneAndEmailVerificationApplication.java
@@ -0,0 +1,15 @@
+package com.bokito.verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class PhoneAndEmailVerificationApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneAndEmailVerificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bokito/verification/PhoneAndEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bokito.verification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneAndEmailVerificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
