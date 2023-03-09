f51e10c34dca159c6760c80ffe5152f24c9d1ee8
Rachid Benkitou <rachidbenkitou1@gmail.com>
2023-03-09 01:03:08 +0100

    adding security config class

--- src/main/java/com/bokito/VerificationWithPhoneAndEmailApplication.java
+++ src/main/java/com/bokito/VerificationWithPhoneAndEmailApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,10 @@ public class VerificationWithPhoneAndEmailApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VerificationWithPhoneAndEmailApplication.class, args);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
     @Bean
     CommandLineRunner run(UserService userService, UserRepository userRepository, RoleRepository roleRepository){
--- src/main/java/com/bokito/user/service/UserServiceImpl.java
+++ src/main/java/com/bokito/user/service/UserServiceImpl.java
@@ -6,19 +6,38 @@ import com.bokito.user.User;
 import com.bokito.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 @Service
 //@AllArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
     @Autowired
     private UserRepository userRepo;
     @Autowired
     private RoleRepository roleRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user= userRepo.findByUsenrame(username);
+        if(user==null){
+            log.error("User not found in the database.");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities= new ArrayList<>();
+        user.getRoles().forEach(role-> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new org.springframework.security.core.userdetails.User(user.getUsenrame(), user.getPassword(), authorities);
+    }
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getName());
@@ -58,4 +77,6 @@ public class UserServiceImpl implements UserService{
     public void deleteRoles(){
         roleRepo.deleteAll();
     }
+
+
 }
